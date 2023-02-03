63629cfbba8ba4e68169d4b3af15a38c6af791a6
Chris Thullen <chris_thullen@yekesys.com>
2023-02-03 07:30:48 -0500

    Display list of integers as bin, oct, hex, or dec

--- pycode/code/filev_utils.py
+++ pycode/code/filev_utils.py
@@ -34,6 +34,30 @@ def txt2loi(s) -> list[int]:
         return []
 
 
+def loi2show(loi: list[int], base: str = "dec") -> list[str]:
+    """
+    Return a list of strings, with each string being
+    the representation of the integer
+    :param base: One of the following: "bin", "oct", "dec", "hex"
+    """
+    if base == "bin":
+        print(f"Converting to {base}")
+        return [bin(i)[2:] for i in loi]
+    elif base == "oct":
+        print(f"Converting to {base}")
+        return [oct(i)[2:] for i in loi]
+    elif base == "dec":
+        print(f"Converting to {base}")
+        return [str(i) for i in loi]
+    elif base == "hex":
+        print(f"Converting to {base}")
+        return [hex(i)[2:].upper() for i in loi]
+    else:
+        print(f"Unknown base: {base}, assume decimal")
+        return [str(i) for i in loi]
+
+    
+
 
 
 if __name__ == "__main__":
@@ -45,4 +69,18 @@ if __name__ == "__main__":
     print(txt2loi(bytes(a, encoding="utf-16")))
     print(txt2loi(bytes(a, encoding="utf-32")))
     print(txt2loi(bytes("123", encoding="ascii")))
+
+
+    print(loi2show(txt2loi("abc"), "bin"))
+    print(loi2show(txt2loi("abc"), "oct"))
+    print(loi2show(txt2loi("abc"), "hex"))
+    print(loi2show(txt2loi("abc"), "dec"))
+    print(loi2show(txt2loi("abc")))
+
+    for s in ["abc", "123", "éàèðα", "א☺ꞅ"]:
+        for b in ["bin", "oct", "hex", "dec"]:
+            print(loi2show(txt2loi(s), b))
+
+
+
 
